3f10690d01688ecd1727eedb015ae71d854d1f84
Vladimir <mendozahum20@gmail.com>
2023-08-21 20:03:06 -0600

    SignUpTest-complete

--- src/main/java/pages/SignUpPage.java
+++ src/main/java/pages/SignUpPage.java
@@ -1,13 +1,85 @@
 package pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SignUpPage extends Page {
+	
+
+	@FindBy(id = "firstName")
+	private WebElement firstNameInput;
+	
+	@FindBy(id = "lastName")
+	private WebElement lastNameInput;
+	
+	@FindBy(id = "phone")
+	private WebElement phoneInput;
+	
+	@FindBy(id = "email")
+	private WebElement emailInput;
+	
+	@FindBy(id = "password")
+	private WebElement passwordInput;
+	
+	@FindBy(id = "confirmPassword")
+	private WebElement confirmPasswordInput;
+	
+	@FindBy(id = "acceptTerms")
+	private WebElement acceptTermsCheckbox;
+	
+	@FindBy(xpath = "//button[text() ='Regístrate']")
+	private WebElement registerBtn;
+	
+	@FindBy(id = "swal2-title")
+	private WebElement successRegisterText;
+	
 
 	public SignUpPage(WebDriver browser) {
 		super(browser);
 		PageFactory.initElements(browser, this);
 	}
+	
+	
+	
+	public void setfirstNameInput(String firstName) {
+		firstNameInput.sendKeys(firstName);
+	}
+	
+	public void setLastNameInput(String lastName) {
+		lastNameInput.sendKeys(lastName);
+	}
+	
+	public void setPhoneInput(String phone) {
+		phoneInput.sendKeys(phone);
+	}
+	
+	public void setEmailInput(String email) {
+		emailInput.sendKeys(email);
+	}
+	
+	public void setPasswordInput(String password) {
+		passwordInput.sendKeys(password);
+	}
+	
+	public void setPasswordConfirmInput(String confirmPassword) {
+		confirmPasswordInput.sendKeys(confirmPassword);
+	}
+	
+	public void checkAcceptTerms() {
+		acceptTermsCheckbox.sendKeys(Keys.SPACE);;
+	}
+	
+	public void clickRegister() {
+		this.findElementAndClick(registerBtn);
+	}
+	
+	public String getSuccessRegisterMessage(String text) {
+		return this.validateTextPresent(successRegisterText, text);
+	}
+	
 
 }
--- src/test/java/testcase/sanity/SignUpUserTest.java
+++ src/test/java/testcase/sanity/SignUpUserTest.java
@@ -1,8 +1,30 @@
 package testcase.sanity;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
 import pages.SignUpPage;
+import pages.home.TopHeaderPage;
 import testCaseImplements.TestCase;
 
 public class SignUpUserTest extends TestCase {
-	SignUpPage signUp = new SignUpPage(browser);
+	
+	@Test
+	public void signUpTest() {
+		TopHeaderPage header = new TopHeaderPage(browser);
+		header.navigateToRegister();
+		
+		
+		SignUpPage signUp = new SignUpPage(browser);
+		signUp.setfirstNameInput("Freddy");
+		signUp.setLastNameInput("Flores");
+		signUp.setPhoneInput("77777799");
+		signUp.setEmailInput("juantest09@gmail.com");
+		signUp.setPasswordInput("12345T3st");
+		signUp.setPasswordConfirmInput("12345T3st");
+		signUp.checkAcceptTerms();
+		signUp.clickRegister();
+		assertEquals(signUp.getSuccessRegisterMessage("Registro exitoso!"), "Registro exitoso!");
+	}
 }
